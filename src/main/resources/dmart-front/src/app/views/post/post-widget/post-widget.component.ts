0bc642e61fe9c1449890fe93f2c01109c1a43130
Youssef TOUNOUSSI <youssef.tounoussi@gmail.com>
2021-06-17 16:38:13 +0100

    Add post widget

--- src/main/resources/dmart-front/src/app/views/post/post-widget/post-widget.component.ts
+++ src/main/resources/dmart-front/src/app/views/post/post-widget/post-widget.component.ts
@@ -1,18 +1,8 @@
-import {
-  Component,
-  ComponentFactoryResolver,
-  Input,
-  OnInit,
-  TemplateRef,
-  ViewChild,
-  ViewContainerRef,
-} from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { SafeResourceUrl } from "@angular/platform-browser";
 import { Comment } from "src/app/models/comment.model";
 import { Post } from "src/app/models/post.model";
 import { CommentService } from "src/app/services/comment/comment.service";
-import { CommentBoxComponent } from "../../comment";
-import { fadeIn, fadeInOut } from "../animations";
 
 @Component({
   selector: "dmart-post-widget",
@@ -36,20 +26,30 @@ export class PostWidgetComponent implements OnInit {
   @Input()
   comments: Comment[];
 
+  // Counter used to handle pageable comments
+  counter: number = 0;
+
   //New comment
-  private newComment: Comment = new Comment();
+  public newComment: Comment = new Comment();
 
-  constructor(
-    private commentService: CommentService,
-    private compFactoryResolver: ComponentFactoryResolver
-  ) {}
+  constructor(private commentService: CommentService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.comments.length <= 3) {
+      this.counter = this.comments.length;
+    } else {
+      this.counter = 3;
+    }
+  }
 
   /**
    *
    */
   submitCommentAfterKeyUpEvent() {
+    if (this.comments.length <= 3) {
+      this.counter = this.comments.length;
+    }
+
     console.log("Comment key up event performed : ");
     this.newComment.pseudoname = this.loadedProfile.pseudoname;
     this.commentService
@@ -60,6 +60,19 @@ export class PostWidgetComponent implements OnInit {
           this.comments.unshift(savedComment);
           this.newComment = new Comment();
         }
+
+        if (this.comments.length <= 3) {
+          this.counter = this.comments.length;
+        }
       });
+  }
+
+  getMoreComments() {
+    if (this.counter <= this.comments.length - 3) {
+      this.counter += 3;
+    } else if (this.counter <= this.comments.length) {
+      this.counter += this.comments.length - this.counter;
+    }
+    console.log(this.counter + " shown comments");
   }
 }
